0c1c3f58f0c354a15625197a65ff17550018a04e
Salmonberry <yshpeng@liangzhicn.com>
2020-07-21 16:09:29 +0800

    feat(update): update

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:inject/inject.dart';
 import 'package:lz_flutter/flutter_base.dart';
 import 'package:lz_flutter_app/proxy.dart';
 import 'package:lz_flutter_app/res/string/en.dart';
 import 'package:lz_flutter_app/res/string/zh.dart';
 
-import 'config/router.dart';
 import 'config/signature_interceptor.dart';
 import 'di/app_injector.dart';
 
 AppInjector injector;
 String apiDomain;
 
-void mainCommon(String apiServerUrl) async {
+Future<void> mainCommon(String apiServerUrl) async {
   apiDomain = apiServerUrl;
   WidgetsFlutterBinding.ensureInitialized();
   await init();
@@ -25,14 +23,14 @@ Future<void> init() async {
   await SpUtil.getInstance(); //初始化SharedPreferences
 
   Config.getInstance().resourceConfig //资源配置
-      .setLocalRes({"zh": zh, "en": en}) //传入 国际化文件
-      .setDefaultLanguageCode("zh"); //国际化文件没有当前手机语言时 显示的默认语言
+      .setLocalRes({'zh': zh, 'en': en}) //传入 国际化文件
+      .setDefaultLanguageCode('zh'); //国际化文件没有当前手机语言时 显示的默认语言
 
   Config.getInstance()
       .netWorkConfig //网络配置
       .setApiDomain(apiDomain) //Api Domain
-      .setProxy(PROXY)
-      .setConnectionTimeout(Duration(seconds: 15)) //设置超时时间
+      .setProxy(proxy)
+      .setConnectionTimeout(const Duration(seconds: 15)) //设置超时时间
       .setJsonConverter(//Json自动序列化
           JsonToTypeConverter(injector.jsonConverter.getJsonConvert()))
       .addNetWorkInterceptor(SignatureInterceptor()); //添加Chopper的拦截器
@@ -40,8 +38,7 @@ Future<void> init() async {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context) => MaterialApp(
       title: 'Flutter Demo',
        routes: injector.router.getRoutersData(injector),
        initialRoute: '/',
@@ -49,5 +46,4 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
     );
-  }
 }
--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -2,5 +2,5 @@
 import 'main_common.dart';
 
 Future<void> main() async {
-  await mainCommon("");
+  await mainCommon('');
 }
