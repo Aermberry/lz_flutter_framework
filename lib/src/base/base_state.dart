40eb143a0ad4dc90b2161649c3a13317260c749c
moyuqin <yqmo@liangzhicn.com>
2020-08-07 16:04:33 +0800

    增加BaseState

---
+++ lib/src/base/base_state.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:lz_flutter/flutter_base.dart';
+import '../widget/default_loading_dialog.dart';
+
+
+abstract class BaseState<T extends StatefulWidget>  extends State<T> implements View{
+
+  @override
+  void showMsgBySnackBar(String msg) {
+    Scaffold.of(getContext()).showSnackBar(SnackBar(content: new Text(msg)));
+  }
+
+  @override
+  void showMsgByToast(String msg) {
+    Fluttertoast.showToast(msg: msg);
+  }
+
+  @override
+  void showLoadingDialog([String msg]) {
+    showDialog(
+        context: getContext(),
+        builder: (context) {
+          return LoadingDialog(
+            text: msg,
+          );
+        });
+
+  }
+
+  @override
+  void hideLoadingDialog(){
+    pop();
+  }
+
+  @override
+  void pop({Object result}) {
+    Navigator.of(getContext()).pop(result);
+  }
+
+  @override
+  void popTo(String routePath){
+    Navigator.of(getContext()).popUntil(ModalRoute.withName(routePath));
+  }
+
+  @override
+  Future<T> routeTo<T extends Object>(String routePath,{bool replace = false,bool clearStack = false,bool keepThisPage = false, Object params}){
+    if(replace){
+      return Navigator.pushReplacementNamed(getContext(), routePath,arguments: params);
+    } else if(clearStack){
+      return Navigator.pushNamedAndRemoveUntil(getContext(), routePath,(r) => keepThisPage, arguments: params);
+    }else{
+      return Navigator.pushNamed(getContext(), routePath,arguments: params);
+    }
+  }
+
+  @override
+  BuildContext getContext() => context;
+
+}
