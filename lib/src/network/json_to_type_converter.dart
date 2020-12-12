f215db362d76a1094d2d7eabf83796755be01bff
moyuqin <yqmo@liangzhicn.com>
2020-12-12 14:49:03 +0800

    commit

--- lib/src/network/json_to_type_converter.dart
+++ lib/src/network/json_to_type_converter.dart
@@ -5,8 +5,9 @@ import 'dart:convert';
 class JsonToTypeConverter extends JsonConverter {
 
   final Map<Type, Function> typeToJsonFactoryMap;
+  final Function defaultTypeFactory;
 
-  JsonToTypeConverter(this.typeToJsonFactoryMap);
+  JsonToTypeConverter(this.typeToJsonFactoryMap,{this.defaultTypeFactory});
 
   @override
   Response convertError<BodyType, InnerType>(Response response) {
@@ -14,6 +15,10 @@ class JsonToTypeConverter extends JsonConverter {
       return response;
     }
     var jsonMap = json.decode(response.body);
+    if(typeToJsonFactoryMap[InnerType] == null){
+      return response.replace(
+          body:defaultTypeFactory(jsonMap));
+    }
     return response.replace(
       body: typeToJsonFactoryMap[InnerType](jsonMap),
     );
@@ -29,13 +34,14 @@ class JsonToTypeConverter extends JsonConverter {
     );
   }
 
-  T fromJsonData<T, InnerType>(String jsonData, Function jsonParser) {
+  T fromJsonData<T, InnerType>(dynamic jsonData, Function jsonParser) {
     dynamic jsonMap;
     try{
       jsonMap = json.decode(jsonData);
     }catch(e){
       return jsonData as T;
     }
+
 
     if (jsonMap is List) {
       return jsonMap.map((item) => jsonParser(item as Map<String, dynamic>) as InnerType).toList() as T;
